51f67e320dbeff6ea36492c1eefac0ad3a7ed369
Subjekt.iv <Agustin@192.168.1.2>
2021-08-01 19:00:25 -0300

    mas mods

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ const app = express()
 const port = 3030
 const path = require('path')
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 4e216a6bdcb998a0a1d864d9718eed98394c9e57
 app.get('/', function(request, response){
     response.sendFile(path.join(__dirname, 'views/index.html'))
 })
@@ -30,10 +27,6 @@ app.get('*', function (request, response){
 })
 
 app.listen(port, ()=>{
-<<<<<<< HEAD
     console.log('La app esta funcionado en http://localhost:'+ port )
-=======
-    console.log('La app esta funcionado en http://localhost:'+ port +"/home")
->>>>>>> 4e216a6bdcb998a0a1d864d9718eed98394c9e57
 })
 
