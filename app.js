353b4e876996b0e0aca60da68234e369a0e180e6
milena-righetti <righettimilena00@gmail.com>
2021-08-06 13:06:40 -0300

    milena righetti

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get('/', function(request, response){
     response.sendFile(path.join(__dirname, 'views/index.html'))
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join (__dirname, 'views/register.html'))
+})
+
+
 app.get('/carrito', function(request, response){
     response.sendFile(path.join(__dirname, 'views/carrito.html'))
 })
@@ -15,13 +20,15 @@ app.get('/market', (req, res) => {
     res.sendFile(path.join (__dirname, 'views/market.html'))
 })
 
+
 app.use(express.static('public'))
 
 app.get('*', function (request, response){
     response.send('NOT FOUND', 404)
 })
 
+
 app.listen(port, ()=>{
-    console.log('La app esta funcionado en http://localhost:'+ port )
+    console.log('La app esta funcionado en http://localhost:3030')
 })
 
